45517d67482e070ec4b904c158dc86bff8a37d63
zhtmr <zhtmr123@gmail.com>
2023-12-11 18:00:33 +0900

    refac: 메뉴 헨들러

---
+++ myapp/app/src/main/java/bitcamp/menu/MenuHandler.java
@@ -0,0 +1,9 @@
+package bitcamp.menu;
+
+// 메뉴를 실행시킬 때 작업을 수행할 객체의 사용 규칙 정의
+public interface MenuHandler {
+
+  // 사용자가 메뉴를 선택하면, MenuItem 객체를 다음 규칙에 따라 호출
+  public abstract void action();
+
+}
--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,7 +1,13 @@
 package bitcamp.myapp;
 
-import bitcamp.myapp.menu.MenuGroup;
-import bitcamp.myapp.menu.MenuItem;
+import bitcamp.menu.MenuGroup;
+import bitcamp.menu.MenuItem;
+import bitcamp.myapp.menu.BoardAddHandler;
+import bitcamp.myapp.menu.BoardDeleteHandler;
+import bitcamp.myapp.menu.BoardListHandler;
+import bitcamp.myapp.menu.BoardModifyHandler;
+import bitcamp.myapp.menu.BoardRepository;
+import bitcamp.myapp.menu.BoardViewHandler;
 import bitcamp.myapp.util.Prompt;
 
 public class App {
@@ -9,6 +15,7 @@ public class App {
   public static void main(String[] args) {
     Prompt prompt = new Prompt(System.in);
 //    new MainMenu(prompt).execute();
+    BoardRepository boardRepository = new BoardRepository();
 
     MenuGroup mainMenu = new MenuGroup("메인");
     MenuGroup assignmentMenu = new MenuGroup("과제");
@@ -25,11 +32,11 @@ public class App {
     assignmentMenu.add(new MenuItem("목록"));
 
     mainMenu.add(boardMenu);
-    boardMenu.add(new MenuItem("등록"));
-    boardMenu.add(new MenuItem("조회"));
-    boardMenu.add(new MenuItem("변경"));
-    boardMenu.add(new MenuItem("삭제"));
-    boardMenu.add(new MenuItem("목록"));
+    boardMenu.add(new MenuItem("등록", new BoardAddHandler(boardRepository, prompt)));
+    boardMenu.add(new MenuItem("조회", new BoardViewHandler()));
+    boardMenu.add(new MenuItem("변경", new BoardModifyHandler()));
+    boardMenu.add(new MenuItem("삭제", new BoardDeleteHandler()));
+    boardMenu.add(new MenuItem("목록", new BoardListHandler(boardRepository)));
 
     mainMenu.add(memberMenu);
     memberMenu.add(new MenuItem("등록"));
