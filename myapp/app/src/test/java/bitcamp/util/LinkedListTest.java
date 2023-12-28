6191cc9b1bd238058ae6939ec2e620410da33044
zhtmr <zhtmr123@gmail.com>
2023-12-28 14:12:52 +0900

    test: LinkedListTest

--- myapp/app/src/test/java/bitcamp/util/LinkedListTest.java
+++ myapp/app/src/test/java/bitcamp/util/LinkedListTest.java
@@ -1,11 +1,30 @@
 package bitcamp.util;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LinkedListTest {
 
+  LinkedList<String> linkedList;
+
+  @BeforeEach
+  void init() {
+    linkedList = new LinkedList<>();
+  }
+
   @Test
   void add() {
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
+
+    assertEquals(3, linkedList.size());
+    assertArrayEquals(new String[]{"A", "B", "C"}, linkedList.toArray());
   }
 
   @Test
@@ -14,66 +33,70 @@ class LinkedListTest {
 
   @Test
   void get() {
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
+
+    assertEquals("B", linkedList.get(1));
   }
 
   @Test
   void set() {
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
+
+    assertEquals("B", linkedList.set(1, "D"));
+    assertArrayEquals(new String[]{"A", "D", "C"}, linkedList.toArray());
   }
 
   @Test
-  void testAdd() {
+  @DisplayName("add(int index, E value)")
+  void testAddAtIndex() {
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
+
+    linkedList.add(1, "D");
+
+    assertEquals(4, linkedList.size());
+    assertArrayEquals(new String[]{"A", "D", "B", "C"}, linkedList.toArray());
   }
 
   @Test
+  @DisplayName("remove(int index)")
   void remove() {
-    LinkedList<String> list = new LinkedList<>();
-    list.add("aaa");
-    list.add("bbb");
-    list.add("ccc");
-    list.add("ddd");
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
 
-    System.out.println(list.remove("xxx"));
-    System.out.println(list.remove("ccc"));
-    System.out.println(list.remove("ddd"));
-    System.out.println(list.remove("aaa"));
-    System.out.println(list.remove("bbb"));
-    list.add("xxx");
-    list.add("yyy");
-    list.add("zzz");
-
-    String[] array = list.toArray(new String[0]);
-    for (Object val : array) {
-      System.out.printf("%s, ", val);
-    }
-    System.out.println();
+    assertEquals("B", linkedList.remove(1));
+    assertEquals(2, linkedList.size());
+    assertArrayEquals(new String[]{"A", "C"}, linkedList.toArray());
   }
 
   @Test
-  void testRemove() {
+  @DisplayName("remove(E value)")
+  void removeByValue() {
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
+
+    assertTrue(linkedList.remove("B"));
+    assertEquals(2, linkedList.size());
+    assertArrayEquals(new String[]{"A", "C"}, linkedList.toArray());
   }
 
   @Test
-  void testToArray() {
-    ArrayList<String> list = new ArrayList<>();
-    list.add("aaa");
-    list.add("bbb");
-    list.add("ccc");
-    list.add("ddd");
+  @DisplayName("toArray(final E[] arr): size 보다 작으면 배열 복사")
+  void toArrayWithArrayArgument() {
+    linkedList.add("A");
+    linkedList.add("B");
+    linkedList.add("C");
 
-    System.out.println(list.remove("xxx"));
-    System.out.println(list.remove("ccc"));
-    System.out.println(list.remove("ddd"));
-    System.out.println(list.remove("aaa"));
-    System.out.println(list.remove("bbb"));
-    list.add("xxx");
-    list.add("yyy");
-    list.add("zzz");
+    String[] ret = linkedList.toArray(new String[0]);
 
-    String[] array = list.toArray(new String[0]);
-    for (Object val : array) {
-      System.out.printf("%s, ", val);
-    }
-    System.out.println();
+    assertArrayEquals(new String[]{"A", "B", "C"}, ret);
   }
 
   @Test
