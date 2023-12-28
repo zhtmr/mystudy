cbae517d4cf7b508c50f9f0d6be401ce598b58dd
zhtmr <zhtmr123@gmail.com>
2023-12-28 15:56:08 +0900

    Refac: Stack 으로 메뉴관리하기

--- myapp/app/src/main/java/bitcamp/menu/MenuGroup.java
+++ myapp/app/src/main/java/bitcamp/menu/MenuGroup.java
@@ -3,6 +3,7 @@ package bitcamp.menu;
 import bitcamp.util.LinkedList;
 import bitcamp.util.List;
 import bitcamp.util.Prompt;
+import bitcamp.util.Stack;
 
 // Composite 패턴에서 '복합 객체(composite object)' 역할을 하는 클래스
 // - 다른 Menu 객체를 포함한다.
@@ -10,16 +11,18 @@ public class MenuGroup extends AbstractMenu {
 
   private List<Menu> menus = new LinkedList<>();
 
-  public MenuGroup(String title) {
-    super(title);
+  public MenuGroup(String title, Stack<String> breadcrumb) {
+    super(title, breadcrumb);
   }
 
   @Override
   public void execute(Prompt prompt) {
+    // 메뉴를 실행할 때 메뉴의 제목을 breadcrumb 에 추가한다.
+    breadcrumb.push(title);
     this.printMenu();
 
     while (true) {
-      String input = prompt.input("%s> ", this.getTitle());
+      String input = prompt.input("%s> ", this.getMenuPath());
 
       if (input.equals("menu")) {
         this.printMenu();
@@ -40,6 +43,8 @@ public class MenuGroup extends AbstractMenu {
         System.out.println("메뉴가 옳지 않습니다");
       }
     }
+
+    breadcrumb.pop(); // 메뉴 나갈때 메뉴 제목을 제거한다.
   }
 
   private void printMenu() {
@@ -54,6 +59,18 @@ public class MenuGroup extends AbstractMenu {
 
   public void add(Menu menu) {
     this.menus.add(menu);
+  }
+
+  public MenuItem addItem(String title, MenuHandler handler) {
+    MenuItem menuItem = new MenuItem(title, handler, breadcrumb);
+    add(menuItem);
+    return menuItem;
+  }
+
+  public MenuGroup addGroup(String title) {
+    MenuGroup menuGroup = new MenuGroup(title, breadcrumb);
+    add(menuGroup);
+    return menuGroup;
   }
 
   public void remove(Menu menu) {
