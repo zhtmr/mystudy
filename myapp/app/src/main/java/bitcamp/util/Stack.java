dcd1f545014653206f6bf5e2d314eae7c04ba055
zhtmr <zhtmr123@gmail.com>
2023-12-28 15:29:16 +0900

    refac: Stack 수정

--- myapp/app/src/main/java/bitcamp/util/Stack.java
+++ myapp/app/src/main/java/bitcamp/util/Stack.java
@@ -4,16 +4,16 @@ public class Stack<E> extends LinkedList<E> {
 
 
   public E push(E value) {
-    super.add(value);
+    add(value);
     return value;
   }
 
   public E pop() {
-    return super.remove(size() - 1);
+    return remove(size() - 1);
   }
 
   public E peek() {
-    return super.get(size() - 1);
+    return get(size() - 1);
   }
 
   public boolean empty() {
