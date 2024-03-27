0f6fd39e9caba5f72dd3af397f016b445ecd95db
csw <susu199037@gmail.com>
2024-03-27 14:28:28 +0900

    Feat: - thymeleaf

--- myapp/app/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -2,12 +2,21 @@ package bitcamp.myapp.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.nio.charset.StandardCharsets;
 
 
 
@@ -24,8 +33,42 @@ public class AppConfig {
     return new StandardServletMultipartResolver();
   }
 
-  @Bean
+//  @Bean
   public ViewResolver viewResolver() {
-    return new InternalResourceViewResolver("/WEB-INF/jsp/", ".jsp");
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/jsp/");
+//    viewResolver.setSuffix(".jsp");
+    viewResolver.setViewNames("*.jsp");
+    viewResolver.setOrder(1);
+    return viewResolver;
+  }
+
+  @Bean
+  public ThymeleafViewResolver thymeleafViewResolver(ISpringTemplateEngine springTemplateEngine) {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(springTemplateEngine);
+    viewResolver.setViewNames(new String[] {"*.html", "*.xhtml", "*"});
+    viewResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    viewResolver.setOrder(2);
+    return viewResolver;
+  }
+
+  @Bean
+  public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext) {
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(applicationContext);
+    templateResolver.setPrefix("/WEB-INF/templates/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCacheable(false);  // 개발하는 동안 html 파일 수정 시 갱신됨
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver);
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
   }
 }
